e0b259c109e220dd72a0a94b50091de6a171732f
Ramiro <ramyvaritis@gmail.com>
2025-03-25 20:47:59 -0300

    primer parte del trabajo backend

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+// Importamos nuestras dependencias
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import process from 'process';
+
+// Creamos nuestra app express
+const app = express();
+// Leemos el puerto de las variables de entorno, si no está, usamos uno por default
+const port = process.env.PORT || 9000;
+
+// Configuramos los plugins
+// Más adelante intentaremos entender mejor cómo funcionan estos plugins
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+
+// Mis endpoints van acá
+app.get('/', (req, res) => {
+    res.json('Hello world');
+});
+
+// Levantamos el servidor en el puerto que configuramos
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
